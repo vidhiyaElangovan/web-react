40f4adf1937e1cfdf567eeed06c99dadcf2f06c3
vidhiya elangovan <vidhiya29@gmail.com>
2019-01-31 19:57:26 +0530

    changes

--- home.jsx
+++ home.jsx
@@ -9,61 +9,57 @@ class LikeButton extends React.Component {
         liked: false,
         customizer : {}
      };
-     this.productDetail = this.productDetail.bind(this);
-    
+     this.productDetail = this.productDetail.bind(this);  
   }
-  productDetail(products){
-        console.log("data", products);
-        let productView = products.map( (product, index) =>{
-           return <div key={index}><h1>{product.promize_customizer_id}</h1>
-           {product.pps.map((section, index) => {
-               return <div key={index}>
-                   <h1>{section.section_name}</h1>
-                   {section.ppt.map((tab, index) => {
-                       return <div key={index}>
-                           <p>{tab.tab_name}</p>
-                       </div>
-                   })}
-               </div> 
 
-            })}
-            </div>
-        })
-
-        return (<div>{productView}</div>);
+  componentWillMount(){
+    var my = {
+        "0": "www.bluesky.com",
+        "1": "6fe973aa2ed56c9a0225b0903360b9ab45e416ba",
+        "2": "848233365551"
+        }
+    const userAction = async () => {
+        const response = await fetch('https://cloudcdn.productimize.com/dcprocld/promize/customproductdetail',{
+                method: 'POST',
+                body: JSON.stringify(my), // string or object
+                headers:{
+                  'Content-Type': 'application/json'
+                }
+              }
+        );
+        const myJson =  await response.json();
+         console.log("myjson",myJson);
+        //this.productDetail(myJson);
+        this.setState({customizer: myJson});
+      }
+  }
+  componentWillReceiveProps(props){
+    console.log("props",props);
   }
 
   render() {
-    if (this.state.liked) {
-        var my = {
-            "0": "www.bluesky.com",
-            "1": "6fe973aa2ed56c9a0225b0903360b9ab45e416ba",
-            "2": "848233365551"
-            }
-        const userAction = async () => {
-            const response = await fetch('https://cloudcdn.productimize.com/dcprocld/promize/customproductdetail',{
-                    method: 'POST',
-                    body: JSON.stringify(my), // string or object
-                    headers:{
-                      'Content-Type': 'application/json'
-                    }
-                  }
-            );
-            const myJson =  await response.json();
-             //extract JSON from the http response
-             console.log("myjson",myJson);
-            // this.setState({customizer: myJson});
-            this.productDetail(myJson);
-          }
-          userAction();
-    }
-    let productView
-    return <div> {e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Get Data'
-    )}
-    </div>
+
+    console.log("this",this.state);
+    const products = this.state.customizer;
+    let productView='';
+    let productView = products.map( (product, index) =>{
+        return <div key={index}><h1>{product.promize_customizer_id}</h1>
+        {product.pps.map((section, index) => {
+            return <div key={index}>
+                <h1>{section.section_name}</h1>
+                {section.ppt.map((tab, index) => {
+                    return <div key={index}>
+                        <p>{tab.tab_name}</p>
+                    </div>
+                })}
+            </div> 
+
+         })}
+         </div>
+     })
+
+     return (<div>{productView}</div>);
+
   }
 }
 
